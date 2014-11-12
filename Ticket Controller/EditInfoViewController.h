0511ef7c3d45303b8601651244bdd17bcff8c77a
Gilberto Villani <giba@MacBook-Air-de-Gilberto.local>
2014-11-12 20:53:37 -0200

    ver 0.2

---
+++ Ticket Controller/EditInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditInfoViewController.h
+//  Ticket Controller
+//
+//  Created by Gilberto Villani on 09/11/14.
+//  Copyright (c) 2014 Infragistics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditInfoViewController : UITableViewController
+
+@end
